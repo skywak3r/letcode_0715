832743cae4d45cc3255d4c560f6474b1d11d184a
skywak3r <502776616@qq.com>
2021-07-15 14:52:06 +0800

    first

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+class Solution(object):
+    def replaceSpace(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        pass
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
