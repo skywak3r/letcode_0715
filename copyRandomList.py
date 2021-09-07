6dbae59507af5399b9c9fca9b752e76ae6298b6e
skywak3r <502776616@qq.com>
2021-09-07 22:26:15 +0800

    剑指 Offer 35.

---
+++ copyRandomList.py
@@ -0,0 +1,83 @@
+# -*- coding:utf-8 -*-
+import copy
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+
+        tmp1 = copy.copy(head)
+        tmpHead = copy.copy(head)
+        while tmp1 != None:
+
+            tmp2 = Node(tmp1.val,tmp1.next,tmp1.random)
+
+            tmp1 = tmp1.next
+        return tmpHead
+
+
+
+        pass
+
+"""
+解法1：
+哈希表
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if not head: return
+        dic = {}
+        # 3. 复制各节点，并建立 “原节点 -> 新节点” 的 Map 映射
+        cur = head
+        while cur:
+            dic[cur] = Node(cur.val)
+            cur = cur.next
+        cur = head
+        # 4. 构建新节点的 next 和 random 指向
+        while cur:
+            dic[cur].next = dic.get(cur.next)
+            dic[cur].random = dic.get(cur.random)
+            cur = cur.next
+        # 5. 返回新链表的头节点
+        return dic[head]
+
+
+解法2：
+拼接 + 拆分
+在源节点后创建新的节点，到时候再将其拆分
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if not head: return
+        cur = head
+        # 1. 复制各节点，并构建拼接链表
+        while cur:
+            tmp = Node(cur.val)
+            tmp.next = cur.next
+            cur.next = tmp
+            cur = tmp.next
+        # 2. 构建各新节点的 random 指向
+        cur = head
+        while cur:
+            if cur.random:
+                cur.next.random = cur.random.next
+            cur = cur.next.next
+        # 3. 拆分两链表
+        cur = res = head.next
+        pre = head
+        while cur.next:
+            pre.next = pre.next.next
+            cur.next = cur.next.next
+            pre = pre.next
+            cur = cur.next
+        pre.next = None # 单独处理原链表尾节点
+        return res      # 返回新链表头节点
+
+
+"""
+
+
+if __name__ == 'main':
+    pass
