1f18f5bab17531559ff46da7873568de0542d394
skywalker <502776616@qq.com>
2022-07-20 23:16:34 +0800

    2022.07.20

--- 树/94. 二叉树的中序遍历.py
+++ 树/94. 二叉树的中序遍历.py
@@ -47,4 +47,29 @@ class Solution:
             res.append(cur.val)
             cur = cur.right
         return res
+
+
+
+官方的
+
+stack 不存
+先将左边全压入
+再访问当前
+
+
+class Solution:
+    def kthLargest(self, root: TreeNode, k: int) -> int:
+        if not root:
+            return
+        res = []
+        node = root
+        stack = []
+        while node or stack:
+            while node:
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            res.append(node.val)
+            node = node.right
+        return res
 """
